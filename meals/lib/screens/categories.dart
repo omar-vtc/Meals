27ce04c4fa73c18f3ddd0897335ea0d3d8b3e16b
Omar Khaled <72626558+omar-vtc@users.noreply.github.com>
2025-05-26 21:27:58 +0300

    added the data and the multi screens callls

---
+++ meals/lib/screens/categories.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+  void _selectCategory(BuildContext context) {
+    // Navigator.push(context, route)
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => MealsScreen(title: "Some title", meals: []),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pick your Category!")),
+      body: GridView(
+        padding: EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          //two columns next to each others
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          //availableCategories.map((category) => CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context);
+              },
+            ),
+        ],
+      ),
+    );
+  }
+}
